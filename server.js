cbb26cf52b614ad9d0dc988bb1bd69210064c859
NKG579 <tag.nikoltag@gmail.com>
2026-02-23 09:21:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
 
 app.use(express.static("public"));
 
 const players = {};
 
 io.on("connection", (socket) => {
-  console.log("Игрок подключился:", socket.id);
 
-  players[socket.id] = { x: 0, y: 2, z: 0 };
+  socket.on("newPlayer", (data) => {
+    players[socket.id] = {
+      x: 0,
+      y: 2,
+      z: 0,
+      nickname: data.nickname
+    };
 
-  socket.emit("currentPlayers", players);
-
-  socket.broadcast.emit("newPlayer", {
-    id: socket.id,
-    player: players[socket.id]
+    socket.emit("currentPlayers", players);
+    socket.broadcast.emit("newPlayer", {
+      id: socket.id,
+      player: players[socket.id]
+    });
   });
 
   socket.on("updatePosition", (data) => {
     if (players[socket.id]) {
-      players[socket.id] = data;
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      players[socket.id].z = data.z;
+
       socket.broadcast.emit("playerMoved", {
         id: socket.id,
-        player: data
+        player: players[socket.id]
       });
     }
   });
 
-  // 💬 ЧАТ
   socket.on("chatMessage", (msg) => {
     io.emit("chatMessage", {
       id: socket.id,
+      nickname: players[socket.id]?.nickname || "Player",
       message: msg
     });
   });
@@ -43,12 +51,9 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("playerDisconnected", socket.id);
-    console.log("Игрок отключился:", socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("Сервер запущен");
 });
